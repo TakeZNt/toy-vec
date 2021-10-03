bb443041fae40f9f03fbbbe81831fc04d0b435ef
Naoto Takezawa <naoto9march@gmail.com>
2021-10-03 15:55:10 +0900

    pushとgetを実装

--- src/lib.rs
+++ src/lib.rs
@@ -36,11 +36,41 @@ impl<T: Default> ToyVec<T> {
         self.elements.len()
     }
 
+    /// 指定インデックスの要素を返す
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if self.length <= index {
+            return None;
+        }
+        Some(&self.elements[index])
+    }
+
+    /// 要素を追加する
+    pub fn push(&mut self, element: T) {
+        if self.length == self.capacity() {
+            self.grow();
+        }
+        self.elements[self.length] = element;
+        self.length += 1;
+    }
+
     fn allocate_in_heap(size: usize) -> Box<[T]> {
         std::iter::repeat_with(Default::default)
             .take(size)
             .collect::<Vec<_>>()
             .into_boxed_slice()
+    }
+
+    fn grow(&mut self) {
+        if self.capacity() == 0 {
+            self.elements = Self::allocate_in_heap(1);
+            return;
+        }
+        // 容量が2倍の配列を確保する
+        let new_elements = Self::allocate_in_heap(self.length * 2);
+        let old_elements = std::mem::replace(&mut self.elements, new_elements); // 配列を置き換える場合に使う
+        for (i, element) in old_elements.into_vec().into_iter().enumerate() {
+            self.elements[i] = element;
+        }
     }
 }
 
@@ -60,5 +90,42 @@ mod tests {
         let toyvec = ToyVec::<usize>::with_capacity(3);
         assert_eq!(0, toyvec.len());
         assert_eq!(3, toyvec.capacity());
+    }
+
+    #[test]
+    fn get() {
+        let toyvec = ToyVec::<usize>::with_capacity(3);
+        assert_eq!(None, toyvec.get(0));
+        assert_eq!(None, toyvec.get(3));
+    }
+
+    #[test]
+    fn push_without_grow() {
+        let mut toyvec = ToyVec::<usize>::with_capacity(3);
+        toyvec.push(100);
+        assert_eq!(Some(&100), toyvec.get(0));
+        assert_eq!(None, toyvec.get(3));
+    }
+
+    #[test]
+    fn push_with_grow01() {
+        let mut toyvec = ToyVec::<usize>::new();
+        toyvec.push(100);
+
+        assert_eq!(Some(&100), toyvec.get(0));
+        assert_eq!(None, toyvec.get(1));
+    }
+
+    #[test]
+    fn push_with_grow02() {
+        let mut toyvec = ToyVec::<usize>::with_capacity(2);
+        toyvec.push(100);
+        toyvec.push(200);
+        toyvec.push(300);
+
+        assert_eq!(Some(&100), toyvec.get(0));
+        assert_eq!(Some(&200), toyvec.get(1));
+        assert_eq!(Some(&300), toyvec.get(2));
+        assert_eq!(None, toyvec.get(3));
     }
 }
